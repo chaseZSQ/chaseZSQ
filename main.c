42485be5bef624e90ce316c50209e56f6c598a4a
chase <shuaiqi.zhang97@gmail.com>
2021-07-14 07:17:22 -0700

    ubuntu

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main (int argc, char **argv)
 {
     printf ("[develop branch]Hello Yocto!\n");
-    printf ("zsq");
+    printf ("zsq_ubuntu");
     return 0;
 } /* ----- End of main() ----- */
 
